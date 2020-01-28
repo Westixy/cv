be781e108561e9f9656f08f28b18c7f18dc274ad
Esteban Sotillo <eso@pop-os.localdomain>
2020-01-28 19:40:29 +0100

    init cv

---
+++ src/App.svelte
@@ -0,0 +1,22 @@
+<script>
+	import CV from './pages/CV.svelte'
+</script>
+
+<main>
+	<CV />
+</main>
+
+<style>
+/* :global(body) {
+	padding: 0px;
+	margin: 0px;
+	background-color: grey;
+}
+main {
+	width: 100vw;
+	height: 100vh;
+	display: flex;
+	align-items: center;
+	justify-items: center;
+} */
+</style>
---
+++ src/pages/CV.svelte
@@ -0,0 +1,428 @@
+<div class="cv-cnt">
+	<div class="cv-konami">((a,ks='38384040373937396665',e=document.body,k=0)=>e.addEventListener('keydown',ke=>+ks.slice(k*2,k*2+2)===ke.keyCode?++k>=ks.length/2?a(e,k=0):null:k=0))(x=>x.style.fontFamily='monospace')</div>
+	<div class="cv-header">
+		<div class="cv-pic">
+			<img
+				src="https://media.licdn.com/dms/image/C4D03AQELc1x5az0NMg/profile-displayphoto-shrink_200_200/0?e=1582761600&v=beta&t=BNeheOmruj4EeOj2vhfiaJgqth0-CBZ2cBMFoCmJwTw"
+				alt="personal picture" />
+			</div>
+		<div class="cv-name">
+			<div class="cv-name-full">Esteban Sotillo</div>
+			<div class="cv-name-title">Software developer / DevOps</div>
+		</div>
+		<div class="cv-personal-info">
+			<div class="cv-personal-info-item">
+				<div class="cv-personal-info-text">Célibataire</div>
+			</div>
+			<div class="cv-personal-info-item">
+				<div class="cv-personal-info-text">Suisse, 23 ans</div>
+			</div>
+			<div class="cv-personal-info-item">
+				<div class="cv-personal-info-text">Rte d'Yverdon 23A<br />1028 Préveranges</div>
+			</div>
+			<div class="cv-personal-info-item">
+				<div class="cv-personal-info-text">079 328 68 69</div>
+			</div>
+			<div class="cv-personal-info-item">
+				<div class="cv-personal-info-text">estebansotillo@netplus.ch</div>
+			</div>
+		</div>
+	</div>
+	<div class="cv-citation">
+		<div class="cv-citation-text">“Intelligence is the ability to avoid doing work, yet getting the work done.”</div>
+		<div class="cv-citation-author">Linus Torvald</div>
+	</div>
+	<div class="cv-content">
+		<div class="cv-col">
+			<div class="cv-card cv-experiences">
+				<div class="cv-card-header">
+					<div class="cv-icon">E</div>
+					<div class="cv-card-title">Expériences</div>
+				</div>
+				<div class="cv-card-content">
+					<div class="cv-card-element">
+						<div class="cv-card-element-before">
+							11.2018 - ...
+						</div>
+						<div class="cv-card-element-title">
+							Software developer / DevOps Jr
+						</div>
+						<div class="cv-card-element-brand">
+							Olympe.ch, EPFL
+						</div>
+						<div class="cv-card-element-content">
+							- Mise en place de CI/CD (Gitlab / Jenkins / bash / awscli)<br/>
+							- Création d'outils de benchmarking (Js)<br/>
+							- Maintient de l'infrastructure IT locale <br/>
+							- Support aux devs<br/>
+						</div>
+					</div>
+					<div class="cv-card-element">
+						<div class="cv-card-element-before">
+							2 mois
+						</div>
+						<div class="cv-card-element-title">
+							Frontend developer Jr
+						</div>
+						<div class="cv-card-element-brand">
+							Infomaniak SA, Genève
+						</div>
+						<div class="cv-card-element-content">
+							- Bug-fixs sur l'interface web (Angular)
+						</div>
+					</div>
+					<div class="cv-card-element">
+						<div class="cv-card-element-before">
+							6 mois
+						</div>
+						<div class="cv-card-element-title">
+							Contract software developer
+						</div>
+						<div class="cv-card-element-brand">
+							FIVB, Lausanne
+						</div>
+						<div class="cv-card-element-content">
+							- Création d'un outil de saisie de statistiques <br/>
+							de match en réseau (NodeJS Electron React node-IPC)<br/>
+						</div>
+					</div>
+				</div>
+			</div>
+			<div class="cv-card cv-trainings">
+				<div class="cv-card-header">
+					<div class="cv-icon">F</div>
+					<div class="cv-card-title">Formations</div>
+				</div>
+				<div class="cv-card-content">
+					<div class="cv-card-element">
+						<div class="cv-card-element-before">
+							2016 - 2018
+						</div>
+						<div class="cv-card-element-title">
+							Technicien ES Développement
+						</div>
+						<div class="cv-card-element-brand">
+							CPNV, Ste-Croix
+						</div>
+						<div class="cv-card-element-content">
+							- Spécialisation développement web / mobile<br/>
+							- Travail de diplôme: <a href="https://frama.link/eso-rapport">https://frama.link/eso-rapport</a>  <br/>
+							- Usage de frameworks mvc (Rails Aurelia) <br/>
+						</div>
+					</div>
+					<div class="cv-card-element">
+						<div class="cv-card-element-before">
+							2012 - 2016
+						</div>
+						<div class="cv-card-element-title">
+							Informaticien CFC
+						</div>
+						<div class="cv-card-element-brand">
+							CFPSion / RETEL Collombey SA
+						</div>
+						<div class="cv-card-element-content">
+							Apprentissage d'informaticien généraliste (Dual)<br/>
+							- Gestion de parcs informatique (PME) <br/>
+							- Développement d'une app web "TMLS"
+						</div>
+					</div>
+				</div>
+			</div>
+			<div class="cv-footer">
+				<div class="cv-col cv-card cv-references">
+					<div class="cv-card-header">
+						<div class="cv-icon">R</div>
+						<div class="cv-card-title">Référence</div>
+					</div>
+					<div class="cv-card-content">
+						<div class="cv-card-element">
+							<div class="cv-card-element-before">
+								FIVB
+							</div>
+							<div class="cv-card-element-title">
+								M. Romain Lanz
+							</div>
+							<div class="cv-card-element-brand">
+								Dev Fullstack
+							</div>
+							<div class="cv-card-element-content">
+								+41 79 960 83 25<br/>
+								romain.lanz@fivb.org<br/>
+							</div>
+						</div>
+						<div class="cv-card-element"></div>
+						<div class="cv-card-element"></div>
+					</div>
+					<div class="cv-card-header">
+						<div class="cv-icon">L</div>
+						<div class="cv-card-title">Langues</div>
+					</div>
+					<div class="cv-card-content">
+						<div class="cv-card-element">
+							<div class="cv-card-element-before">
+								&nbsp;
+							</div>
+							<div class="cv-card-element-content">
+								Français (maternelle)<br/>
+								Anglais (B1)<br/>
+								Espagnol (B1)<br/>
+							</div>
+						</div>
+						<div class="cv-card-element"></div>
+						<div class="cv-card-element"></div>
+					</div>
+				</div>
+				<div class="cv-col cv-card cv-additional-info">
+					<div class="cv-card-header">
+						<div class="cv-icon">P</div>
+						<div class="cv-card-title">Projets</div>
+					</div>
+					<div class="cv-card-content">
+						<div class="cv-card-element">
+							<div class="cv-card-element-before">
+								React Riot
+							</div>
+							<div class="cv-card-element-title">
+								Gallyt
+							</div>
+							<div class="cv-card-element-brand">
+								Git light web client
+							</div>
+							<div class="cv-card-element-content">
+								Fait en équipe pour le React Riot <br/>
+								<a href="http://2018.reactriot.com/entries/188-es-community">https://frama.link/gallyt</a>
+							</div>
+						</div>
+						<div class="cv-card-element">
+							<div class="cv-card-element-before">
+								Github
+							</div>
+							<div class="cv-card-element-title">
+								Westixy
+							</div>
+							<div class="cv-card-element-brand">
+								Github account
+							</div>
+							<div class="cv-card-element-content">
+								Compte github contenant mes principaux projets open sources <br/>
+								<a href="https://github.com/Westixy?tab=repositories">https://github.com/Westixy</a>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+		<div class="cv-col" style="width:220px">
+			<div class="cv-card cv-technos">
+				<div class="cv-card-header">
+					<div class="cv-icon">T</div>
+					<div class="cv-card-title">Technos</div>
+				</div>
+				<div class="cv-card-content">
+					<div class="cv-card-element">
+						<div class="cv-card-element-before">
+							DevOps
+						</div>
+						<div class="cv-card-element-content">
+							bash Docker awscli AWS-SDK Ansible gopass Prometheus Grafana Gitlab Jenkins Git nix-shell
+						</div>
+					</div>
+					<div class="cv-card-element">
+						<div class="cv-card-element-before">
+							Web stack
+						</div>
+						<div class="cv-card-element-content">
+							ESNext NodeJS Svelte React Webpack AdonisJs PWA WebWorkers Electron
+						</div>
+					</div>
+				</div>
+			</div>
+			<div class="cv-card cv-interests">
+				<div class="cv-card-header">
+					<div class="cv-icon">I</div>
+					<div class="cv-card-title">Intérêts</div>
+				</div>
+				<div class="cv-card-content">
+					<div class="cv-card-element">
+						<div class="cv-card-element-before">
+							ES-Community
+						</div>
+						<div class="cv-card-element-content">
+							ES-Community est un groupe de développeur dont l'objectif est de fournir un lieu d'échange, d'entraide et de veille technologique autour de l'écosystème ECMAScript.<br/>
+							<a href="https://github.com/ES-Community/">https://github.com/ES-Community/</a>
+						</div>
+					</div>
+					<div class="cv-card-element">
+						<div class="cv-card-element-before">
+							Enthousiaste
+						</div>
+						<div class="cv-card-element-content">
+							Rust WebAssembly Machine-learning Sapper Infrastructure-as-Code Code-is-art DevSecOps
+						</div>
+					</div>
+					<div class="cv-card-element">
+						<div class="cv-card-element-before">
+							Insomni'Hack
+						</div>
+						<div class="cv-card-element-content">
+							L'Insomni'Hack est un événement sur la sécuritué informatique se déroulant tous les ans à Genève. Un contest de pentesting y est organisé.
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
+
+<style>
+.cv-konami{
+    position: absolute;
+    width: 98vw;
+    left: 0;
+    top: 0;
+    font-size: 69px;
+    overflow: hidden;
+    word-break: break-all;
+    opacity: 0.01;
+    font-family: monospace;
+}
+.cv-card {
+    margin-top: 20px;
+}
+.cv-cnt {
+    /* max-width: 720px;
+    margin: auto; */
+    padding: 15px;
+    background: white;
+    /* box-shadow: 0px 2px 4px 0px #0000001f; */
+}
+
+.cv-pic>img {
+    border-radius: 1000px;
+    border: 2px solid var(--mc);
+    width: 120px;
+    box-sizing: border-box;
+}
+
+.cv-header {
+    display: flex;
+    align-items: center;
+	margin-bottom: 20px;
+}
+
+.cv-name {
+    flex: auto;
+    padding: 0px 10px;
+}
+
+:root {
+    --mc: #4472d9;
+}
+
+.cv-personal-info-item {
+    display: flex;
+    align-items: start;
+    padding-bottom: 3px;
+}
+
+.cv-personal-info-text {
+    padding-left: 10px;
+    border-left: 2px solid var(--mc);
+    margin-bottom: 1px;
+	font-size: small;
+	border-radius: 2px;
+}
+
+.cv-personal-info {
+    padding-right: 10px;
+}
+
+.cv-name-full {
+    color: var(--mc);
+    font-size: xx-large;
+}
+
+.cv-name-title {
+    font-size: x-large;
+}
+
+.cv-card-header {
+    display: flex;
+    padding: 5px;
+    align-items: center;
+}
+
+.cv-icon {
+    display: block;
+    box-sizing: border-box;
+    padding-top: 5px;
+    height: 30px;
+    width: 30px;
+    font-size: large;
+    background-color: var(--mc);
+    color: white;
+    text-align: center;
+    border-radius: 1000px;
+}
+
+.cv-card-title {margin-left: 10px;font-size: x-large;}
+
+.cv-card-element {
+    position: relative;
+    padding-left: 46px;
+	margin-bottom: 10px;
+}
+
+.cv-card-element-before {
+    position: absolute;
+    left: 0;
+	top:0px;
+    height: 100%;
+    transform-origin: 100% 50%;
+    transform: rotate(180deg);
+    writing-mode: vertical-lr;
+    text-align: center;
+    box-sizing: border-box;
+    font-size: x-small;
+    border-right: 2px solid var(--mc);
+    padding-left: 3px;
+    padding-right: 3px;
+    color: var(--mc);
+	border-radius: 2px;
+}
+
+.cv-card-element-title {
+    display: inline-block;
+    color: var(--mc);
+	font-size: large;
+}
+
+.cv-card-element-brand {
+    display: inline-block;
+    padding-left: 10px;
+	font-size: small;
+}
+.cv-card-element-brand::before {
+    content: '-';
+    position: absolute;
+    margin-left:-8px;
+}
+.cv-content, .cv-footer{
+	display: flex;
+}
+.cv-col{flex:auto}
+
+.cv-citation {
+	text-align: center;
+	color: var(--mc);
+	font-size: large;
+	font-style: italic;
+	opacity:.8;
+}
+.cv-citation-author {
+	text-align: right;
+	padding-right: 30px;
+	font-size: medium;
+}
+
+</style>
